f54c26550454452fdd3244e9b9ad280fb7469d9c
kisenka <qtuzov@gmail.com>
2016-09-22 13:01:51 +0300

    Initial plugin implementation

--- packages/docpack-jsdoc-extractor/lib/plugin.js
+++ packages/docpack-jsdoc-extractor/lib/plugin.js
@@ -1,14 +1,34 @@
 var docpack = require('docpack');
 var extractor = require('./extractor');
+var Promise = require('bluebird');
+var _readFile = require('webpack-toolkit/lib/readFile');
 
-module.exports = docpack.createPlugin('jsdoc-extractor', {
-  apply: function(compiler) {
-    var extractorPlugin = this;
+module.exports = docpack.createPlugin('jsdoc-extractor', function(compiler) {
+  compiler.plugin('compilation', function(compilation) {
+    compilation.plugin(docpack.HOOKS.EXTRACT, function(sources, done) {
+      var promises = Promise.map(sources, function(source) {
 
-    compiler.plugin(docpack.HOOKS.INIT, function(docpack) {
-      docpack.registerExtractor(extractorPlugin);
+        var context = {
+          fs: compilation.compiler.inputFileSystem,
+          compilation: compilation,
+          module: compilation.modules.filter(function (module) {
+            return module.resource == source.absolutePath
+          })[0],
+          addDependency: function (filepath) {
+            this.module.fileDependencies.push(filepath);
+          },
+          readFile: function (filepath) {
+            return _readFile(this.fs, filepath);
+          }
+        };
+
+        return extractor.call(context, source);
+      });
+
+      Promise.all(promises).then(function(s) {
+        done(null, s);
+      });
+
     });
-  },
-
-  extract: extractor
+  });
 });
