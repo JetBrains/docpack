701fb712b4ace3f51f7c40b11463c8b8bb07c9bf
kisenka <qtuzov@gmail.com>
2016-09-26 23:37:21 +0300

    Update deps

--- packages/docpack-jsdoc-extractor/lib/plugin.js
+++ packages/docpack-jsdoc-extractor/lib/plugin.js
@@ -4,19 +4,24 @@ var extractor = require('./extractor');
 var findModule = require('webpack-toolkit').getModuleByFilepath;
 var Promise = require('bluebird');
 
-module.exports = docpack.createPlugin('jsdoc-extractor', function(compiler) {
-  compiler.plugin('compilation', function(compilation) {
-    compilation.plugin(docpack.HOOKS.EXTRACT, function(sources, done) {
+module.exports = docpack.createPlugin({
+  name: 'jsdoc-extractor',
+  apply: function (compiler) {
 
-      var promises = Promise.map(sources, function (source) {
-        var module = findModule(compilation, source.absolutePath);
-        var context = createContext(compiler.inputFileSystem, module);
-        return extractor.call(context, source);
-      });
+    compiler.plugin('compilation', function (compilation) {
+      compilation.plugin(docpack.HOOKS.EXTRACT, function (sources, done) {
 
-      Promise.all(promises).then(function(sources) {
-        done(null, sources);
+        var promises = Promise.map(sources, function (source) {
+          var module = findModule(compilation, source.absolutePath);
+          var context = createContext(compiler.inputFileSystem, module);
+          return extractor.call(context, source);
+        });
+
+        Promise.all(promises).then(function (sources) {
+          done(null, sources);
+        });
       });
     });
-  });
+
+  }
 });
