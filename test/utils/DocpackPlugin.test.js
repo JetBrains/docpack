e3ee9413da9cd79339d608f238e8a562859278c8
kisenka <qtuzov@gmail.com>
2016-09-26 16:16:28 +0300

    Fix renamed paths

--- test/utils/DocpackPlugin.test.js
+++ test/utils/DocpackPlugin.test.js
@@ -1,4 +1,4 @@
-var DocpackPlugin = require('../lib/utils/docpackPlugin');
+var DocpackPlugin = require('../../lib/utils/DocpackPlugin');
 var create = DocpackPlugin.create;
 
 describe('Docpack Plugin', () => {
