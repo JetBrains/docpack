64ae452422a2b89dc030d08280fa55d9d08f69ef
kisenka <qtuzov@gmail.com>
2017-02-13 00:19:23 +0300

    Add current env saving into .env file

--- tasks/select-env.js
+++ tasks/select-env.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const fs = require('fs');
 const shell = require('shelljs');
 const utils = require('./utils');
 const envs = require('../env.json');
@@ -11,3 +12,5 @@ packages.forEach(pkg => {
   shell.exec('npm link');
   shell.exec(`lerna exec --concurrency 1 -- npm link ${pkg.split('@')[0]}`);
 });
+
+fs.writeFileSync(path.resolve(__dirname, '../.env'), env, 'utf-8');
