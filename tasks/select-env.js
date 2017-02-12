18a868263554ea88bd65c799087a137e68d6ed55
kisenka <qtuzov@gmail.com>
2017-02-13 00:27:59 +0300

    Update env chooser script

--- tasks/select-env.js
+++ tasks/select-env.js
@@ -5,12 +5,15 @@ const utils = require('./utils');
 const envs = require('../env.json');
 const env = utils.getCLIArgs()['_'][0];
 const packages = envs[env];
+const rootDir = path.resolve(__dirname, '..');
 
 packages.forEach(pkg => {
-  const dest = path.resolve(__dirname, `../node_modules/${pkg}`);
+  const dest = path.resolve(rootDir, `node_modules/${pkg}`);
+
   shell.cd(dest);
   shell.exec('npm link');
-  shell.exec(`lerna exec --concurrency 1 -- npm link ${pkg.split('@')[0]}`);
+  shell.cd(rootDir);
+  shell.exec(`./node_modules/.bin/lerna exec --concurrency 1 -- npm link ${pkg.split('@')[0]}`);
 });
 
 fs.writeFileSync(path.resolve(__dirname, '../.env'), env, 'utf-8');
